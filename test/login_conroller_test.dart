81196a487a3154fb3466c7317285c0d248a54f1e
kinivera <kiaravelasquez123@gmail.com>
2023-06-01 01:41:42 -0500

    more fixes

--- test/login_conroller_test.dart
+++ test/login_conroller_test.dart
@@ -1,9 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
+import 'package:greenplastic_app/ui/controllers/database_controller.dart';
 import 'package:greenplastic_app/ui/controllers/login_controller.dart';
 
-void main() {
+void main() async {
   TestWidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: 'AIzaSyAUKTtAMncwayflb2hkvlUVkF39CKvt5KA',
+    appId: '1:529378076268:android:1fe89868e9237579da9a49',
+    messagingSenderId: '529378076268',
+    projectId: 'greenplasticapp',
+    databaseURL: 'https://greenplasticapp-default-rtdb.firebaseio.com',
+    storageBucket: 'greenplasticapp.appspot.com',
+  ));
+  Get.put(DatabaseController());
+  Get.put(LoginController());
   group('LoginController', () {
     late LoginController loginController;
 
